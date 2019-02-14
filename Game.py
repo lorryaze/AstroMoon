2f538944c13fbf213de3dad76ed32fdeda6a8b55
lorryaze <lorrany90@gmail.com>
2019-02-14 01:12:19 -0200

    Image directory

--- Game.py
+++ Game.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
+import os, sys
+import getopt
+
+images_dir = os.path.join( "/home/lorrany/AstroMoon/", "images" )
+image_fog= "fog.png"
 
 import pygame
 from pygame.locals import *
+
 
 class Background:
 
     #attribute that will be a background of the game
     image = None
+    pos = None
 
-    #constructor for a instance of Background
     def __init__( self ):
-        #image that will be set to the background of game
-        #screen = pygame.display.get_surface()
+
+       # screen = pygame.display.get_surface()
         screen = pygame.display.set_mode((600,600)).convert()
+
         #Create a empty pygame surface and convert surface to make blitting faster
         back = pygame.Surface(screen.get_size())
         #This function fill a background color, a python color is represent for a tuple, the (0,0,0) is black.
@@ -31,8 +38,11 @@ class Game:
 
     def __init__(self):
         pygame.init()
-        logo_icon = pygame.image.load("fog.png")
+
+        logo_icon = os.path.join(images_dir, image_fog)
+        logo_icon = pygame.image.load(logo_icon)
         pygame.display.set_icon(logo_icon)
+
         pygame.display.set_caption("AstroMoon")
 
     def handle_events(self):
