dee1c0511b252cea911a07d04ae29dd2c0c53e52
lorryaze <lorrany90@gmail.com>
2019-02-14 02:23:40 -0200

    Firts background

--- Game.py
+++ Game.py
@@ -4,6 +4,7 @@ import getopt
 
 images_dir = os.path.join( "/home/lorrany/AstroMoon/", "images" )
 image_fog= "fog.png"
+night_back = "tile640.png"
 
 import pygame
 from pygame.locals import *
@@ -17,14 +18,20 @@ class Background:
 
     def __init__( self ):
 
-       # screen = pygame.display.get_surface()
-        screen = pygame.display.set_mode((600,600)).convert()
+        pygame.init()
+        #logo
+        logo_icon = os.path.join(images_dir, image_fog)
+        logo_icon = pygame.image.load(logo_icon)
+        pygame.display.set_icon(logo_icon)
+        pygame.display.set_caption("AstroMoon")
 
-        #Create a empty pygame surface and convert surface to make blitting faster
-        back = pygame.Surface(screen.get_size())
-        #This function fill a background color, a python color is represent for a tuple, the (0,0,0) is black.
-        back.fill((0, 0, 0))
-        self.image = back
+        #screen
+        screen = pygame.display.set_mode((600,600))
+        screen.fill((255,255,255))
+        nigth = os.path.join(images_dir, night_back)
+        nigth = pygame.image.load(nigth)
+        screen.blit(nigth, (0,0))
+        pygame.display.flip()
 
     #def draw(self, screen):
      #   screen.blit(self.image, (0,0))
@@ -35,15 +42,6 @@ class Game:
     screen_size = None
     running = True
     background = None
-
-    def __init__(self):
-        pygame.init()
-
-        logo_icon = os.path.join(images_dir, image_fog)
-        logo_icon = pygame.image.load(logo_icon)
-        pygame.display.set_icon(logo_icon)
-
-        pygame.display.set_caption("AstroMoon")
 
     def handle_events(self):
         for event in pygame.event.get():
